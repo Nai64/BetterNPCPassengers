b946ea68ddab5221e0baf3df7d1add11c10db313
Nai64 <nai64@users.noreply.github.com>
2026-05-12 00:34:19 +0500

    Bump version to v2.7.18

--- lua/nai_npc_passengers/settings.lua
+++ lua/nai_npc_passengers/settings.lua
@@ -2,7 +2,7 @@ NPCPassengers = NPCPassengers or {}
 NPCPassengers.Modules = NPCPassengers.Modules or {}
 NPCPassengers.Modules.settings = true
 
-NPCPassengers.Version = NPCPassengers.Version or "2.7.17"
+NPCPassengers.Version = NPCPassengers.Version or "2.7.18"
 NPCPassengers.BuildDate = NPCPassengers.BuildDate or "2026-05-12"
 
 NPCPassengers.GetConVarBool = NPCPassengers.GetConVarBool or function(name, default)
